df63c8845241e59bd92031786fd35609485dfb96
amtrindade <amtrindade@gmail.com>
2012-11-22 23:11:19 -0200

    change email

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -4,8 +4,8 @@ import org.openqa.selenium.WebDriver;
 public class LoginPage {
 
 	public void login(WebDriver driver){
-		driver.findElement(By.name("Email")).sendKeys("cwi.teste1@gmail.com");
-		driver.findElement(By.name("Passwd")).sendKeys("cwi.teste1");
+		driver.findElement(By.name("Email")).sendKeys("email@gmail.com");
+		driver.findElement(By.name("Passwd")).sendKeys("email");
 		driver.findElement(By.name("signIn")).click();
 	}
 }
