adbf5df6430e459433fca5acfae75ead05603a61
amtrindade <amtrindade@gmail.com>
2013-02-22 11:20:17 -0300

    New test with message wrong login

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -3,21 +3,18 @@ import org.openqa.selenium.WebDriver;
 
 public class LoginPage {
 
-	public String login(WebDriver driver){
-	
-		driver.findElement(By.name("Email")).sendKeys("email@gmail.com");
-		driver.findElement(By.name("Passwd")).sendKeys("email");
-		driver.findElement(By.name("signIn")).click();
-		
-		for (int second = 0;; second++) {
-			if (second >= 60) fail("timeout");
-			try { if ("Nenhum e-mail novo.".equals(driver.findElement(By.cssSelector("td.TC")).getText())) 
-				break; 
-			} 
-			catch (Exception e)	{}
-			Thread.sleep(1000);
-		}
+	public void login(WebDriver driver) {
 
-		result = driver.findElement(By.cssSelector("td.TC")).getText();
+		driver.findElement(By.name("Email")).sendKeys("cwi.teste1@gmail.com");
+		driver.findElement(By.name("Passwd")).sendKeys("cwi.teste1");
+		driver.findElement(By.name("signIn")).click();
+	}
+	
+	public void loginWrongPassword(WebDriver driver) {
+		
+		driver.findElement(By.id("Email")).sendKeys("cwi.teste1@gmail.com");
+	    driver.findElement(By.id("Passwd")).clear();
+	    driver.findElement(By.id("Passwd")).sendKeys("teste");
+	    driver.findElement(By.id("signIn")).click();
 	}
 }
--- src/LoginTest.java
+++ src/LoginTest.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 
+import java.util.concurrent.TimeUnit;
+
 import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -14,22 +16,44 @@ public class LoginTest {
 
 	@Before
 	public void setup() {
+		
 		loginPage = new LoginPage();
 
 		driver = new FirefoxDriver();
 		driver.get("http://www.gmail.com");
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
 
 	@Test
-	public void loginTest() throws InterruptedException {
+	public void loginTest() throws Exception {
 
-		String MessageNothing = loginPage.login(driver);	
+		loginPage.login(driver);	
+		
+		for (int second = 0;; second++) {
+			if (second >= 60) fail("timeout");
+			try { if ("Nenhum e-mail novo.".equals(driver.findElement(By.cssSelector("td.TC")).getText())) 
+				break; 
+			} 
+			catch (Exception e)	{}
+			Thread.sleep(1000);
+		}
+
+		String MessageNothing =  driver.findElement(By.cssSelector("td.TC")).getText();		
 		assertEquals("Nenhum e-mail novo.",	MessageNothing);
+	}
+	
+	@Test
+	public void shouldMessageLoginWrong() throws Exception {
+		
+		loginPage.loginWrongPassword(driver);	
+		
+		assertTrue(driver.findElement(By.cssSelector("BODY")).getText().matches("^[\\s\\S]*Nome de usuário incorreto ou senha incorreta[\\s\\S]*$"));
 	}
 
 	@After
 	public void tearDown() throws Exception {
 		driver.quit();
+		
 		String verificationErrorString = verificationErrors.toString();
 		if (!"".equals(verificationErrorString)) {
 			fail(verificationErrorString);
